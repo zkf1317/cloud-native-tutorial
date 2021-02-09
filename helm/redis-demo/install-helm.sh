c80cf448090f71962d59675c84976978e918d140
zkf1317 <kfzhang82@126.com>
2021-02-09 16:03:14 +0800

    install script bug fix

--- helm/redis-demo/install-helm.sh
+++ helm/redis-demo/install-helm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add –
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
